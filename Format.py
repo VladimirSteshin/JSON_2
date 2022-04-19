849e2a4c6d34aac11074d41cafd72a72a0ff715c
VladimirSteshin <warheim24@gmail.com>
2022-04-19 21:03:28 +0300

    Solved

--- Format.py
+++ Format.py
@@ -1,12 +1,17 @@
 import json
 from pprint import pprint
+
 with open('group_people.json', 'r') as file:
     work = json.load(file)
 counting_people = {}
 for i in work:
     group = i['id_group']
-    persons = []
+    persons = 0
     for val in i['people']:
         if val['gender'].lower() == 'female' and val['year'] >= 1977:
-            persons.append(val)
+            persons += 1
     counting_people[group] = persons
+winner = max(counting_people.values())
+for key, value in counting_people.items():
+    if value == winner:
+        print(key, value)
